a2756756069be45a471aaae31b726b670fa7ddcb
Bruno Siecola Bombonate <bruno.bombonate@megaleios.com>
2020-02-21 14:42:04 -0300

    public module improvements

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -17,25 +17,25 @@ import { FintechComponent } from './containers/fintech/fintech.component';
 import { InformationsComponent } from './containers/informations/informations.component';
 import { PublicationsComponent } from './containers/publications/publications.component';
 import { PressComponent } from './containers/press/press.component';
-import { SponsorExhibitComponent } from './containers/sponsor-exhibit/sponsor-exhibit.component';
 import { KeynoteSpeakersComponent } from './containers/keynote-speakers/keynote-speakers.component';
+import { SponsorExhibitComponent } from './containers/sponsor-exhibit/sponsor-exhibit.component';
 import { SubscriptionComponent } from './containers/subscription/subscription.component';
 
 // components
 import { SliderComponent } from './components/slider/slider.component';
 import { SliderNavComponent } from './components/slider/slider-nav.component';
 import { ModalVideoComponent } from './components/modal-video/modal-video.component';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
-import { SponsorExhibitFormComponent } from './components/sponsor-exhibit-form/sponsor-exhibit-form.component';
 import { KeynoteSpeakerComponent } from './components/keynote-speaker/keynote-speaker.component';
 import { KeynoteSpeakerModalComponent } from './components/keynote-speaker-modal/keynote-speaker-modal.component';
-import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { BannerSponsorExhibitComponent } from './components/banner-sponsor-exhibit/banner-sponsor-exhibit.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { GalleryNavComponent } from './components/gallery/gallery-nav.component';
-import { BannerSponsorExhibitComponent } from './components/banner-sponsor-exhibit/banner-sponsor-exhibit.component';
+import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PageHeaderMenuComponent } from './components/page-header-menu/page-header-menu.component';
+import { SponsorExhibitFormComponent } from './components/sponsor-exhibit-form/sponsor-exhibit-form.component';
 import { ModalPhotosComponent } from './components/modal-photos/modal-photos.component';
 import { ModalRegisterFormComponent } from './components/modal-register-form/modal-register-form.component';
-import { PageHeaderMenuComponent } from './components/page-header-menu/page-header-menu.component';
 import { ModalTermsUseComponent } from './components/modal-terms-use/modal-terms-use.component';
 import { ModalPrivacyPolicyComponent } from './components/modal-privacy-policy/modal-privacy-policy.component';
 
@@ -50,25 +50,24 @@ import { ModalPrivacyPolicyComponent } from './components/modal-privacy-policy/m
     InformationsComponent,
     PublicationsComponent,
     PressComponent,
+    KeynoteSpeakersComponent,
     SponsorExhibitComponent,
+    SubscriptionComponent,
     // components
     SliderComponent,
     SliderNavComponent,
     ModalVideoComponent,
-    PageHeaderComponent,
-    SponsorExhibitFormComponent,
-    KeynoteSpeakersComponent,
-    KeynoteSpeakersComponent,
     KeynoteSpeakerComponent,
     KeynoteSpeakerModalComponent,
-    SponsorsComponent,
+    BannerSponsorExhibitComponent,
     GalleryComponent,
     GalleryNavComponent,
-    BannerSponsorExhibitComponent,
+    SponsorsComponent,
+    PageHeaderComponent,
+    PageHeaderMenuComponent,
+    SponsorExhibitFormComponent,
     ModalPhotosComponent,
     ModalRegisterFormComponent,
-    SubscriptionComponent,
-    PageHeaderMenuComponent,
     ModalTermsUseComponent,
     ModalPrivacyPolicyComponent
   ],
@@ -91,8 +90,8 @@ import { ModalPrivacyPolicyComponent } from './components/modal-privacy-policy/m
     InformationsComponent,
     PublicationsComponent,
     PressComponent,
-    SponsorExhibitComponent,
     KeynoteSpeakersComponent,
+    SponsorExhibitComponent,
     SubscriptionComponent
   ]
 })
